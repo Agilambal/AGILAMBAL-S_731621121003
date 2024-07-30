4b87370132be97972b142597fc797358cd495c95
Agilambal Stheswaran <agilambal5sgmail.com>
2024-07-30 18:47:45 +0530

    changes Angular

--- Angular/MyAngularProject1/src/app/app.component.ts
+++ Angular/MyAngularProject1/src/app/app.component.ts
@@ -6,21 +6,50 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name:string;
-  age:number;
-  email:string;
-  imagePath:string;
+//   name:string;
+//   age:number;
+//   email:string;
+//   imagePath:string;
+//   fname:string;
+
+//   constructor(){
+//     this.name="Agila";
+//     this.age=20;
+//     this.email="agila@gmail.com";
+//     this.imagePath="./assets/Ang1.png";
+//     this.fname=" ";
+//   }
+//   change(){
+//     this.name="vijay";
+//   }
+//   changeImage(){
+//     this.imagePath="./assets/Ang1.png";
+//   }
+// }
+
+
+  flag:boolean;
+  vehicles:string[];
+  selectedVehicle:string;
+  myStyle:{};
+  myClass:string;
 
   constructor(){
-    this.name="Agila";
-    this.age=20;
-    this.email="agila@gmail.com";
-    this.imagePath="./assets/Ang1.png";
+    this.flag=true;
+    this.vehicles=["Two Wheeler","Three Wheeler","Four Wheeler"];
+    this.selectedVehicle="";
+    this.myStyle={'width':'40%','border':'2px solid green','border-radius':'15px'};
+    this.myClass="myClass1";
   }
-  change(){
-    this.name="vijay";
+  changeFlag(){
+    this.flag=!this.flag;
+    
   }
-  changeImage(){
-    this.imagePath="./assets/Ang1.png";
+  setSelectedItem(vec:string){
+    this.selectedVehicle=vec;
   }
-}
+  changeStyle(){
+    this.myStyle={'width':'40%','border':'2px solid red','border-radius':'15px'};
+  
+  }
+  }
